228d5d3b8f443bdc415175825ac6805d22a69b5d
marce <marce@192.168.0.9>
2016-12-15 18:21:47 -0200

    Testing Database Connection

---
+++ src/Exercicios/TestConnection.java
@@ -0,0 +1,24 @@
+package Exercicios;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+/**
+ *Objective: To test ConnectionFactory
+ * @author marcella
+ */
+public class TestConnection 
+{
+    
+    public static void main (String[] args)
+    {
+        try {
+            Connection connection = new ConnectionFactory().getConnection();
+            System.out.println("Connection Opened");
+            connection.close();
+            System.out.println("Connection Closed");
+        } catch (SQLException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+}
