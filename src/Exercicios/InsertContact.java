4cdd589f3a97d9c2e01593c5e7b538d564af9f32
marce <marce@192.168.0.9>
2017-01-02 18:13:59 -0200

    Testing Insert

---
+++ src/Exercicios/InsertContact.java
@@ -0,0 +1,43 @@
+package Exercicios;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Calendar;
+
+/**
+ *Objective: Try an insert
+ * @author marcella
+ */
+public class InsertContact 
+{
+    public static void main (String [] args) 
+    {
+        try(Connection conn = new ConnectionFactory().getConnection()) {
+            
+        
+
+        String sql = "insert into contatos (nome, email, endereco, datanasc)"
+                   + "values(?,?,?,?)";
+
+        PreparedStatement pstmt = conn.prepareStatement(sql);
+        
+        pstmt.setString(1, "Caelum");
+        pstmt.setString(2, "contato@caelum.com.br");
+        pstmt.setString(3, "R. Vergueiro 3185");
+        pstmt.setDate(4, new java.sql.Date(Calendar.getInstance().getTimeInMillis()));
+        
+        pstmt.execute();
+        pstmt.close();
+        
+        System.out.print("Gravado!");
+        
+        conn.close();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        
+        
+        
+    }
+}
